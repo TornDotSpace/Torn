9f37a7be26ed9fb9f7dcf5163d8efc592f9cbb92
John C. Allwein <john.allwein57@gmail.com>
2019-11-09 20:09:19 -0700

    deploy: enable 'production' optimizations for production builds

--- deploy/webpack.prod.js
+++ deploy/webpack.prod.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common.js');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
-	//mode: 'production',
+	mode: 'production',
 	plugins: [
 		new webpack.DefinePlugin({
 			GAMESERVER_URL: '"torn.space:443"',
