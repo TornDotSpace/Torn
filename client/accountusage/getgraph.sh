5bebbadc8e93e58189fa07217f0b6425081874d1
2swap <mchontz10@gmail.com>
2020-01-16 16:00:35 -0500

    Slightly debugged account grapher

---
+++ client/accountusage/getgraph.sh
@@ -0,0 +1,7 @@
+sudo rm accusagedata.txt;
+sudo rm data.json;
+sudo grep -h "logged in as" ../../logs/* | cut -d'!' -f1 | cut -d' ' -f6,2 | sort | uniq | tee accusagedata.txt;
+sudo echo -n "data = '{\"data\":\"" >> data.json;
+sudo sed -z 's/\n/ /g' accusagedata.txt >> data.json;
+sudo echo "\"}'" >> data.json;
+sudo rm accusagedata.txt;
