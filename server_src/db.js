20f6a8a25a275692abe420b283bf2cf641d6845c
John C. Allwein <admin@johnnyapol.me>
2020-02-20 20:53:57 -0500

    mongo: fix execption from invalid username

--- server_src/db.js
+++ server_src/db.js
@@ -39,7 +39,7 @@ global.loadPlayerData = async function (playerName, passwordHash, socket) {
     // Check if player exists in MongoDB (if we're using MongoDB)
     var record = await PLAYER_DATABASE.findOne({ _id: playerName });
 
-    if (record["password"] !== passwordHash) {
+    if (record == null || record["password"] !== passwordHash) {
         return { error: -1}; // Invalid credentials
     }
 
