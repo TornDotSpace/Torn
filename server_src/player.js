f0faa1434fe067b56078ebf3d2bc8845d1ea8bea
2swap <mchontz10@gmail.com>
2020-04-12 16:38:15 -0400

    Fixed a semi-bug with swap

--- server_src/player.js
+++ server_src/player.js
@@ -357,6 +357,8 @@ function Player(sock) {
 		if(self.rank < self.ship) return;
 		if(self.disguise > 0 || self.shield) return;
 		if (self.ship == 16) { // Elite Raider
+			if(self.charge<0) return;
+			self.charge = 0;
 			//This effectively just shoots turbo.
 			var mult = ((self.e || self.gyroTimer > 0) && self.w && (self.a != self.d)) ? 1.025 : 1.017;
 			self.speed *= mult;
