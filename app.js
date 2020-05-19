df0270eceb81efabc69170398d90fb03f2b116ca
2swap <mchontz10@gmail.com>
2020-05-19 15:45:11 -0400

    game mechanic changes

--- app.js
+++ app.js
@@ -128,12 +128,12 @@ global.baseMap=	{
 global.bulletWidth = 16; // collision radius
 var mineLifetime = 3; // mines despawn after this many minutes
 global.botDespawnRate = 0.0005; // Probability a bot with no nearby enemies despawns each tick
-global.baseHealth = 1500; // max base health
-global.baseKillExp = 3000; // Exp reward for killing a base
+global.baseHealth = 2000; // max base health
+global.baseKillExp = 5000; // Exp reward for killing a base
 global.baseKillMoney = 200000; // ditto but money
 global.mapSz = 9; // How many sectors across the server is. If changed, see planetsClaimed
 global.sectorWidth = 14336; // must be divisible by 2048.
-global.moneyPerRaidPoint = 120000;
+global.moneyPerRaidPoint = 200000;
 global.playerLimit = 130; // A soft limit on the max number of players+bots+guests online. When reached, bots do not spawn as much
 
 //Machine Learning
@@ -247,8 +247,8 @@ function updateQuests() {
 				nm = { type: "Delivery", metal: metals[Math.floor(r * 4)], exp: Math.floor(1 + Math.sqrt(square(sxv - dsxv) + square(syv - dsyv))) * 20000, sx: sxv, sy: syv, dsx: dsxv, dsy: dsyv };
 			}
 			else if (i < 7) nm = { type: "Mining", metal: metals[Math.floor(r * 4)], exp: 65000, amt: Math.floor(1200 + r * 400), sx: thisMap[Math.floor(r2 * basesPerTeam) * 2], sy: thisMap[Math.floor(r2 * basesPerTeam) * 2 + 1] };
-			else if (i < 9) nm = { type: "Base", 	exp: 250000, sx: baseMap[whatTeam][Math.floor(r2 * basesPerTeam) * 2], sy: baseMap[whatTeam][Math.floor(r2 * basesPerTeam) * 2 + 1] };
-			else 			nm = { type: "Secret", 	exp: 500000, sx: baseMap[whatTeam][Math.floor(r2 * basesPerTeam) * 2], sy: baseMap[whatTeam][Math.floor(r2 * basesPerTeam) * 2 + 1] };
+			else if (i < 9) nm = { type: "Base", 	exp: 400000, sx: baseMap[whatTeam][Math.floor(r2 * basesPerTeam) * 2], sy: baseMap[whatTeam][Math.floor(r2 * basesPerTeam) * 2 + 1] };
+			else 			nm = { type: "Secret", 	exp: 750000, sx: baseMap[whatTeam][Math.floor(r2 * basesPerTeam) * 2], sy: baseMap[whatTeam][Math.floor(r2 * basesPerTeam) * 2 + 1] };
 			teamQuests[teamColor][i] = nm;
 		}
 	}
@@ -457,7 +457,7 @@ function endRaid() {
 	if (raidRed > raidBlue && raidRed > raidGreen) winners = "red";
 	else if (raidBlue > raidRed && raidBlue > raidGreen) winners = "blue";
 	else if (raidGreen > raidRed && raidGreen > raidBlue) winners = "green";
-	raidTimer = 120*1000;
+	raidTimer = 100*1000;
 	var winnerPoints = Math.max(raidGreen, Math.max(raidBlue, raidRed));
 	for (var i in sockets) {
 		var p = getPlayer(i);
