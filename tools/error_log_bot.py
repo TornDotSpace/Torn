6dd099b184599061fafc127a465a8dc3d2fa2bf7
John C. Allwein <git@johnnyapol.me>
2020-06-21 21:38:10 -0400

    tools: minor changes to error_log_bot

--- tools/error_log_bot.py
+++ tools/error_log_bot.py
@@ -2,24 +2,29 @@ from os import listdir, remove
 from os.path import isfile
 from discord_webhook import DiscordWebhook
 
-data = ""
-
 PATH = "error_logs"
 
-for f in listdir(PATH):
-    print(f)
-    print(isfile(PATH + "/" + f))
-    f = open(PATH + "/" + f, 'r')
-    if isfile(f.name):
-        data = data + f.read()
-        remove(f.name)
-    else:
-        continue
+def send_webhook(msg):
+    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/699745801924247582/BJKcA2Dpa5I_ghWJ979BQFqMkVRTFdzihcF_nkJv9UyEJb0TsBVMn4UiXD36UZK-Ch8U', content=f'```bash\n{msg}```')
+    response = webhook.execute()
 
-if len(data) != 0:
-    # We need to post to discord
-    print(data)
-    for x in [data[i:i+1985] for i in range(0, len(data), 1985)]:
-        webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/699745801924247582/BJKcA2Dpa5I_ghWJ979BQFqMkVRTFdzihcF_nkJv9UyEJb0TsBVMn4UiXD36UZK-Ch8U', content='```bash\n' + x + "```")
-        response = webhook.execute()
-        print(response)
+def main():
+    for f in listdir(PATH):
+        f = open(PATH + "/" + f, 'r')
+        if isfile(f.name):
+            data = data + f.read()
+            remove(f.name)
+        else:
+            continue
+
+    if len(data) != 0:
+        # We need to post to discord
+        responses = 0
+        for x in [data[i:i+1985] for i in range(0, len(data), 1985)]:
+            responses = responses + 1
+            send_webhook(x)
+
+        if (responses == 0):
+            send_webhook("No errors encountered during last run.")
+
+main()
