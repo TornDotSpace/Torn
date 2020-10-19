2cac5601ee894d60bb1b85b306cc95632b033c3d
2swap <mchontz10@gmail.com>
2020-10-19 14:09:27 -0400

    Mine fix and tahu fix

--- server_src/player_mp.js
+++ server_src/player_mp.js
@@ -220,8 +220,10 @@ class PlayerMP extends Player {
       if (!this.guest && !(other_ip !== undefined && other_ip == this.ip)) { // Only award them if their IP differs and they didn't kill a guest
     	    if (this.color !== b.owner.color) b.owner.spoils('experience', 10 + Math.min(b.owner.experience*2,diff));
     	    else b.owner.spoils('experience', -5 * Math.min(diff, b.owner.experience*playerKillExpFraction)); // Punishment equals -5 times what the reward would have been, unless it's large in proportant to the punished person's exp
-        	b.owner.spoils('money', playerKillMoneyFraction*this.money + (b.owner.type === 'Player' ? b.owner.killStreak*playerKillMoney : playerKillMoney));
-        this.money -= playerKillMoneyFraction*this.money;
+        	const moneyEarned = playerKillMoneyFraction*this.money;
+          moneyEarned = Math.max(0,moneyEarned);
+          b.owner.spoils('money', moneyEarned + (b.owner.type === 'Player' ? b.owner.killStreak*playerKillMoney : playerKillMoney));
+        this.money -= moneyEarned;
     	}
 
       if (this.color === b.owner.color && b.owner.type === 'Player') b.owner.save(); // prevents people from logging out to get rid of their punishment
