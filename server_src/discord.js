c5e9bc70fdc3f0d4e40bf68d6c90126a6128c634
tardisfromtornspace <androemdavirgoa@gmail.com>
2020-10-25 18:05:45 +0100

    Trying to see if this works

--- server_src/discord.js
+++ server_src/discord.js
@@ -41,7 +41,8 @@ client.on("message", async message => {
   } else if(args[0] === "/mute") {
     client.channels.cache.get('766664211581239326').send("You must either use /modmute or /ipmute!");
   } else if(args[0] === "/broadcast") {
-    chatAll("~`#f66~`       BROADCAST: ~`lime~`" + msg.substring(11));
+    chatAll("~`#f66~`       BROADCAST: ~`lime~`" + message.content.trim());
+    //chatAll("~`#f66~`       BROADCAST: ~`lime~`" + message.content.trim().substring(11));
     client.channels.cache.get('766664211581239326').send("Message broadcasted.");
   } else if(args[0] === "/reboot") {
   	if(!message.member.roles.cache.some(r=>["Developer"].includes(r.name)))
