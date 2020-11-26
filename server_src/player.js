5ec2b98b2fa1f5d183070b873b47a68239483b24
tardisfromtornspace <androemdavirgoa@gmail.com>
2020-11-26 20:37:22 +0100

    Fixed a few problems with Supercharger, cloak and turbo

--- server_src/player.js
+++ server_src/player.js
@@ -131,13 +131,13 @@ class Player {
     // timer business
     if (this.killStreakTimer-- < 0) this.killStreak = 0; // Sensitive to off-by-ones.
     if (this.borderJumpTimer > 0) this.borderJumpTimer--;
-    this.superchargerTimer--;
+    if (this.superchargerTimer >= 0) this.superchargerTimer--;
     if (this.empTimer > 0) this.empTimer--;
     if (this.disguise > 0) this.disguise--;
 
     const amDrifting = this.e || this.gyroTimer > 0;
     this.shield = (this.s && !amDrifting && this.gyroTimer < 1) || this.leaveBaseShield > 0;
-    if (this.disguise>0 || (this.shield && this.weapons[this.equipped]>0 && wepns[this.weapons[this.equipped]].type !== "Misc" && wepns[this.weapons[this.equipped]].type !== "Mine" && this.space)) this.charge=Math.min(this.charge, 0);
+    if ((this.disguise>0 && this.weapons[this.equipped] != 18 && this.weapons[this.equipped] != 19 && this.weapons[this.equipped] != 21 && this.weapons[this.equipped] != 22 && this.weapons[this.equipped] != 29 && this.weapons[this.equipped] != 36)|| (this.shield && this.weapons[this.equipped]>0 && wepns[this.weapons[this.equipped]].type !== "Misc" && wepns[this.weapons[this.equipped]].type !== "Mine" && this.space)) this.charge=Math.min(this.charge, 0);
     this.leaveBaseShield--;
 
     if (!this.isBot) {
