4f00bae7a84207a773521f41cc319edffb1e26a3
Damien Vesper <ldamienvesper@gmail.com>
2021-04-18 08:44:57 -0500

    core: ts build fixes

--- client_src/chat.ts
+++ client_src/chat.ts
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { Socket } from "socket.io";
-import { translate } from "./localizer.ts";
+import { translate } from "./localizer";
 
 
 /* eslint-disable */
