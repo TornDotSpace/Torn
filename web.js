7fc44c1ffe2c86b75282cc7f817ba5f608b8e82b
Damien Vesper <ldamienvesper@gmail.com>
2021-04-18 09:04:39 -0500

    lint: dont lint ts
    babel has major trouble with typecasting syntax

--- web.js
+++ web.js
@@ -19,9 +19,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const path = require("path");
+
 console.log("\n\nWeb Server started");
 app.use(cors());
-app.use("/", express.static(`${__dirname}/client`));
+app.use("/", express.static(path.resolve(__dirname, "client")));
 const httpServer = http.Server(app);
 httpServer.listen(parseInt(process.argv[2]));// normal is 8443, dev 7301
 console.log("Express started");
