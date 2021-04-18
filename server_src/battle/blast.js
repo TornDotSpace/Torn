436a5a124633b7b1b741e0a0722f31141808fcea
Damien Vesper <ldamienvesper@gmail.com>
2021-04-18 10:46:31 -0500

    clean up blast

--- server_src/battle/blast.js
+++ server_src/battle/blast.js
@@ -15,38 +15,53 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-module.exports = class Blast {
-    constructor (ownr, i, weaponID) {
-        this.type = `Blast`,
-        this.id = i, // unique identifier
-        this.dmg = wepns[weaponID].damage,
-        this.sx = ownr.sx,
-        this.sy = ownr.sy,
-        this.owner = ownr,
-        this.angle = ownr.angle,
-        this.bx = ownr.x,
-        this.by = ownr.y,
-        this.wepnID = weaponID,
-        this.time = 0; // since spawn
+class Blast {
+    constructor (owner, id, weaponID) {
+        this.type = `Blast`;
+        this.id = id; // Unique identifier.
+
+        this.dmg = wepns[weaponID].damage;
+
+        this.sx = owner.sx;
+        this.sy = owner.sy;
+
+        this.owner = owner;
+
+        this.angle = owner.angle;
+        
+        this.bx = owner.x;
+        this.by = owner.y;
+
+        this.weaponID = weaponID;
+        this.time = 0; // Time since weapon spawn.
     }
 
     tick () {
         this.time++;
+
         if (this.time > 11) delete blasts[this.sy][this.sx][this.id];
-        if (this.time == 1) {
+        else if (this.time === 1) {
             for (const i in players[this.sy][this.sx]) {
                 const player = players[this.sy][this.sx][i];
                 if ((this.bx - player.x) * Math.cos(this.angle) + (this.by - player.y) * Math.sin(this.angle) > 0) continue;
+
                 const pDist = Math.hypot(player.x - this.bx, player.y - this.by);
-                const fx = player.x - Math.cos(this.angle) * pDist; // all this ugly math is just to check collision of a player with a ray
+
+                // Calculate beam hitbox.
+                const fx = player.x - Math.cos(this.angle) * pDist;
                 const fy = player.y - Math.sin(this.angle) * pDist;
+
+                // If player hitbox collides with beam hitbox, then have an effect on the player.
                 if (Math.hypot(fx - this.bx, fy - this.by) < ships[player.ship].width * 2 / 3) this.hit(player);
             }
-            if (this.wepnID != 25) {
+
+            if (this.weaponID !== 25) {
                 for (const i in asts[this.sy][this.sx]) {
                     const ast = asts[this.sy][this.sx][i];
                     if ((this.bx - ast.x) * Math.cos(this.angle) + (this.by - ast.y) * Math.sin(this.angle) > 0) continue;
+
                     const pDist = Math.hypot(ast.x - this.bx, ast.y - this.by);
+
                     const fx = ast.x - Math.cos(this.angle) * pDist;
                     const fy = ast.y - Math.sin(this.angle) * pDist;
                     if (Math.hypot(fx - this.bx, fy - this.by) < 64 * 2 / 3) ast.dmg(this.dmg, this); // hits the asteroid.
@@ -54,18 +69,24 @@ module.exports = class Blast {
             }
 
             const base = bases[this.sy][this.sx];
-            if (base.color == this.owner.color || base.baseType == DEADBASE) return;
+
+            if (base.color === this.owner.color || base.baseType === DEADBASE) return;
             if ((this.bx - base.x) * Math.cos(this.angle) + (this.by - base.y) * Math.sin(this.angle) > 0) return;
+
             const pDist = Math.hypot(base.x - this.bx, base.y - this.by);
+
             const fx = base.x - Math.cos(this.angle) * pDist;
             const fy = base.y - Math.sin(this.angle) * pDist;
+
             if (Math.hypot(fx - this.bx, fy - this.by) < 128 * 2 / 3) this.hit(base);
         }
     }
 
     hit (b) {
-        if (this.wepnID == 25 && this.owner.color !== b.color) b.EMP(126); // emp blast
-        else if ((this.wepnID == 34 || this.wepnID == 47) && this.owner.color !== b.color) b.dmg(this.dmg, this); // muon and lepton
-        else if (this.wepnID == 41) b.brainwashedBy = this.owner.id; // brainwashing laser
+        if (this.weaponID === 25 && this.owner.color !== b.color) b.EMP(126); // emp blast
+        else if ((this.weaponID === 34 || this.weaponID === 47) && this.owner.color !== b.color) b.dmg(this.dmg, this); // muon and lepton
+        else if (this.weaponID === 41) b.brainwashedBy = this.owner.id; // brainwashing laser
     }
 };
+
+module.exports = Blast;
