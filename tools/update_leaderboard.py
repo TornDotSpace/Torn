800070baaf84b40b4d84b3348c917e283a592a51
John C. Allwein <git@johnnyapol.me>
2021-04-21 23:42:53 -0400

    leaderboard: improve last updated timestamp
    Add timezone, refactor to remove seconds.

--- tools/update_leaderboard.py
+++ tools/update_leaderboard.py
@@ -141,11 +141,12 @@ def updateLB(conn_str, path):
                 </td><td>{(int(teamdata[key]["rank"]/teamdata[key]["players"]))}</td><td>{int(teamdata[key]["kills"]/teamdata[key]["players"])}\
                     </td><td>{int(teamdata[key]["money"]/teamdata[key]["players"])}</td><td>{(int(teamdata[key]["tech"]/teamdata[key]["players"]))}</td></tr>'
 
+    time = datetime.datetime.now()
     newFile = f'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\
             <meta charset="utf-8"/><html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en"><head><title>Leaderboard</title>\
                 <link rel="stylesheet" href="../page.css" /></head><body><br><br><h1><div style="padding: 20px"><center><font color="#0099ff">\
                 Leaderboard</font></center></div></h1><font color="#0099ff"><center><nobr><table>{teamFile}<tr><td>---</td></tr>{playerFile}\
-                </table></nobr><br/>Last updated: {datetime.datetime.now()}</center></font></body></html>'
+                </table></nobr><br/>Last updated: {time.strftime("%m/%d/%y %H:%M")} {time.astimezone().tzinfo.tzname(None)}"</center></font></body></html>'
     # Write out
     lb = open(path, "w")
     lb.write(newFile)
