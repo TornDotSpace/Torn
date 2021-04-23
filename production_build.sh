9e837f683608a8e5d4c39b23a8596a0845749691
John C. Allwein <git@johnnyapol.me>
2021-04-22 20:29:04 -0400

    deploy: invoke index_cache_bust in test, prod builds

--- production_build.sh
+++ production_build.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env sh
 # Build for torn.space
 echo Building Torn client 
 npm ci && npm run build
+./tools/index_cache_bust.sh
 echo Done.
--- test_build.sh
+++ test_build.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env sh
 # Build for test.torn.space
 echo Building Torn client 
 npm ci &&  npm run test-build
+./tools/index_cache_bust.sh
 echo Done.
