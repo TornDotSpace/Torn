d9127e4b494e5fce70a13d6c77abe0444cd1aeed
Damien Vesper <ldamienvesper@gmail.com>
2021-06-13 22:50:23 -0500

    use full ESM import standard

--- client_src/index.jsx
+++ client_src/index.jsx
@@ -14,11 +14,11 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import ReactRoot from "./react/root";
-import { jsn, translate } from "./localizer.ts";
+import ReactRoot from './react/root';
+import { jsn, translate } from './localizer';
 
 `use strict`;
 
