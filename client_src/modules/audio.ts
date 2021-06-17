9f1c18b7c229b0823bd1483aa3e27f5654187efd
Damien Vesper <ldamienvesper@gmail.com>
2021-06-17 16:19:53 -0500

    client: howler is unused now

--- client_src/modules/audio.ts
+++ client_src/modules/audio.ts
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { Howl, Howler } from 'howler';
+import { Howl } from 'howler';
 
 declare const login: any;
 declare const gVol: any;
