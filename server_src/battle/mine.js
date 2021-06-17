197cdfed231d3cc002a85d3dacfe4cd712b61e35
tardisfromtornspace <androemdavirgoa@gmail.com>
2021-06-17 03:21:52 +0200

    Adjusted teh backlash range to a more normal range

--- server_src/battle/mine.js
+++ server_src/battle/mine.js
@@ -236,7 +236,7 @@ class Mine {
         }
         for (const i in players[this.sy][this.sx]) {
             const p = players[this.sy][this.sx][i];
-            if (squaredDist(p, this) < square(100)) {
+            if (squaredDist(p, this) < square(80)) {
                 p.dmg(this.dmg / 10, this); // if i'm in range of a player on explosion, damage them
                 if (this.wepnID == 17) p.EMP(110); // EMP mine
             }
