3cbe3a9848b529a4136d2d072190e0a4776cf947
2swap <mchontz10@gmail.com>
2021-06-26 11:28:51 -0700

    halve experience script

--- tools/halve_experience.py
+++ tools/halve_experience.py
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ######################################
 # Date: 06/26/2021
-# Purpose: Decay all players. To be run weekly.
+# Purpose: Halve all players' experiences
 #####################################
 
 MONGO_CONNECTION_STR = "mongodb://localhost:27017/torn"
@@ -36,26 +36,10 @@ for player in players.find():
     total += 1
     print(f"Processing: {player['_id']}")
 
-    #if their timestamp is not in unix milliseconds, convert it
-    if type(player['lastLogin']) == datetime:
-        player['lastLogin'] = int(round(player['lastLogin'].timestamp()*1000))
-
-    # Don't run for paid players
-    if player['tag'] == 'V' or player['tag'] == 'B':
-        continue
-
-    #If they played in the last week, don't decay them
-    if ms-player['lastLogin'] < 604800000:
-        continue
-
-    experience = player['experience']*.99
-    money = player['money']*.99
+    experience = player['experience']*.5
 
     # Remove name field and set the tag
     players.update_one(
-        {"_id": player["_id"]}, {"$set": {"experience": experience}, "$set": {"money": money}}
+        {"_id": player["_id"]}, {"$set": {"experience": experience}}
     )
-
-    print(f"    Decayed {player['_id']}")
-    decayed += 1
 print(f"decayed {decayed}/{total}")
