62087ab07de2d948dbd3714271f80ba16469758a
2swap <mchontz10@gmail.com>
2021-06-27 13:10:43 -0700

    Missing file license headers

--- server_src/universe/asteroid.js
+++ server_src/universe/asteroid.js
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2021  torn.space (https://torn.space)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const isOutOfBounds = (obj) => // TODO this works but I'm not even using it anywhere. it would simplify some code if used.
     obj.x < 0 || obj.y < 0 || obj.x >= sectorWidth || obj.y >= sectorWidth;
 
