36b16b8ae1028aabd4d7459b7cc750f29bb83f68
Damien Vesper <ldamienvesper@gmail.com>
2021-06-27 16:29:08 -0500

    client: expToRank is a global array

--- client_src/utils/helper.ts
+++ client_src/utils/helper.ts
@@ -18,6 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import { translate } from '../localizer';
 
 declare const sectorWidth: number;
+declare const expToRank: number[];
+
 declare const bigNotes: any[];
 declare const wepns: any;
 
